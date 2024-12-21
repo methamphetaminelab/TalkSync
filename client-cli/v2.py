44e02f1e6c9bdaf2489ab19b99c21d4858c87106
methamphetaminelab <58376312+methamphetaminelab@users.noreply.github.com>
2024-12-21 15:04:15 +0300

    client-cli

--- client-cli/v2.py
+++ client-cli/v2.py
@@ -15,7 +15,7 @@ style = Style.from_dict({
 })
 
 commands = WordCompleter(
-    ["::connect", "::disconnect", "::send", "::whisper", "::chatmode", "::help", "::exit"],
+    ["connect", "disconnect", "send", "whisper", "chatmode", "help", "exit"],
     ignore_case=True
 )
 
@@ -29,28 +29,28 @@ async def main():
         while True:
             try:
                 command = await session.prompt_async(
-                    "> ",
+                    ">>> ",
                     completer=commands,
                     style=style,
                     complete_in_thread=True,
                 )
 
-                if command == "::connect":
+                if command == "connect":
                     websocket = await connect(session)
                 elif command == "disconnect":
                     await disconnect(websocket)
-                elif command == "::send":
+                elif command == "send":
                     text = await session.prompt_async("Enter message: ")
                     await sendGlobalMessage(websocket, text)
-                elif command == "::whisper":
+                elif command == "whisper":
                     receiver_id = await session.prompt_async("Enter receiver ID: ")
                     text = await session.prompt_async("Enter message: ")
                     await sendPrivateMessage(websocket, text, receiver_id)
-                elif command == "::chatmode":
+                elif command == "chatmode":
                     await chatMode(websocket, session)
-                elif command == "::help":
+                elif command == "help":
                     await printHelp()
-                elif command == "::exit":
+                elif command == "exit":
                     print("\nExiting...")
                     break
                 else:
@@ -121,7 +121,7 @@ async def chatMode(websocket, session: PromptSession):
     try:
         print("-*- Chat mode enabled -*-\n-*- (::chatmode to disable) -*-")
         while True:
-            text = await session.prompt_async("#> ")
+            text = await session.prompt_async("::: ")
             if text == "::chatmode":
                 print("-*- Chat mode disabled -*-")
                 break
@@ -133,13 +133,13 @@ async def chatMode(websocket, session: PromptSession):
 async def printHelp():
     try:
         print(
-            "   ::connect - Connect to the server\n"
-            "   ::disconnect - Disconnect from the server\n"
-            "   ::send - Send a global message\n"
-            "   ::chatmode - Enable chat mode\n"
-            "   ::whisper - Send a private message\n"
-            "   ::help - Show this help message\n"
-            "   ::exit - Exit the client\n"
+            "   connect - Connect to the server\n"
+            "   disconnect - Disconnect from the server\n"
+            "   send - Send a global message\n"
+            "   chatmode - Enable chat mode\n"
+            "   whisper - Send a private message\n"
+            "   help - Show this help message\n"
+            "   exit - Exit the client\n"
         )
     except Exception as e:
         print(e)
